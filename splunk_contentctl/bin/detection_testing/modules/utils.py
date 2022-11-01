8bb7f2c54e2b8411a28cfa3812e8d849cc34b107
pyth0n1c <87383215+pyth0n1c@users.noreply.github.com>
2022-11-01 15:37:17 -0700

    small changes to utils that let us work not just with http(s) files for attack_data, but with local files as well.

--- splunk_contentctl/bin/detection_testing/modules/utils.py
+++ splunk_contentctl/bin/detection_testing/modules/utils.py
@@ -1,30 +1,61 @@
 import os
+import shutil
 import requests
 import random
 import string
+from timeit import default_timer
+import pathlib
+import datetime
 
+TOTAL_BYTES = 0
+TOTAL_DOWNLOAD_TIME = 0
+def download_file_from_http(file_path:str, destination_file:str, overwrite_file:bool=False, chunk_size:int=1024*1024, verbose_print:bool=False)->None:
+    global TOTAL_BYTES, TOTAL_DOWNLOAD_TIME
+    
+    try:
+        if pathlib.Path(file_path).is_file():
+            shutil.copyfile(file_path, destination_file)
+            return
+    except Exception as e:
+        print(f"Could not copy local file {file_path} the file because {str(e)}")
 
-def download_file_from_http(url:str, destination_file:str, overwrite_file:bool=False, chunk_size:int=1024*1024, verbose_print:bool=False)->None:
+    
     if os.path.exists(destination_file) and overwrite_file is False:
         print(f"[{destination_file}] already exists...using cached version")
         return
     if verbose_print:
         print(f"downloading to [{destination_file}]...",end="", flush=True)
     try:
-        file_to_download = requests.get(url, stream=True)
+        download_start_time = default_timer()
+        bytes_written=0
+        file_to_download = requests.get(file_path, stream=True)
         if file_to_download.status_code != 200:
             if verbose_print:
                 print("FAILED")
-            raise Exception(f"Error downloading the file {url}: Status Code {file_to_download.status_code}")
+            raise Exception(f"Error downloading the file {file_path}: Status Code {file_to_download.status_code}")
         with open(destination_file, "wb") as output:
             for piece in file_to_download.iter_content(chunk_size=chunk_size):
-                output.write(piece)
+                bytes_written += output.write(piece)
         if verbose_print:
             print("Done")
+        download_stop_time = default_timer()
+        timedelta = download_stop_time-download_start_time
+        TOTAL_BYTES += bytes_written
+        TOTAL_DOWNLOAD_TIME += timedelta
+        
     except Exception as e:
         if verbose_print:
             print("FAILED")
         raise e
+    return
+
+
+def print_total_download_stats()->None:
+    
+    time_string = datetime.timedelta(seconds=round(TOTAL_DOWNLOAD_TIME))
+    print(f"Download statistics:\n"
+          f"\tTotal MB     :{(TOTAL_BYTES/(1024*1024)):.0f}MB\n"
+          f"\tTotal Seconds:{time_string}s")
 
 # taken from attack_range
 def get_random_password(password_min_length: int = 16, password_max_length: int = 26) -> str:

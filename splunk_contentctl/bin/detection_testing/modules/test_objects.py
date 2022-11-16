a999e7fb2ab91c7d58c21e1fc6978e9d63436056
pyth0n1c <87383215+pyth0n1c@users.noreply.github.com>
2022-11-15 21:21:16 -0800

    More updates to results manager and fixing the generation of the summary output file

--- splunk_contentctl/bin/detection_testing/modules/test_objects.py
+++ splunk_contentctl/bin/detection_testing/modules/test_objects.py
@@ -4,69 +4,37 @@ import yaml
 import os
 import json
 import timeit
-from datetime import timedelta
+from datetime import timedelta, datetime
 
 from bin.objects.detection import Detection
 from bin.objects.unit_test_result import UnitTestResult
 
 
-
-
-
-class DetectionResult:
-    def __init__(self, detection:Detection):
-        self.detection = detection
-        
-    
-    def add_test_result(self, result:UnitTestResult):
-        self.test_results.append(result)
-    
-    def get_total_time(self)->timedelta:
-        runtimes = [result.get_time() for result in self.test_results]
-        total_time = timedelta(0)
-        for res in self.test_results:
-            total_time += res.get_time()
-        return total_time
-    
-    def get_success(self)->bool:
-        if len(self.test_results) == 0:
-            #If there have been no successful tests, then we cannot say anything was successful
-            return False
-        for res in self.test_results:
-            if res.get_success is False:
-                #If a single test Failed, then return failure
-                return False
-            
-        #If we get here, then that means there was at least 1 test and
-        #none of them failed, so success!
-        return True
-
-    def get_num_results(self)->int:
-        return len(self.test_results)
-
 class ResultsManager:
     def __init__(self):
-        self.detectionResults:list[DetectionResult] = []
+        self.detections:list[Detection] = []
 
         # These are important for a running tally. Final summarization and 
         # output will independently calcualte these, though
         self.result_count = 0
         self.pass_count = 0
         self.fail_count = 0
+        self.start_time = datetime.now()
+        
 
 
-    def addCompletedDetection(self, detectionResult:DetectionResult):
+    def addCompletedDetection(self, detection:Detection):
         #Record the overall result of the detection
         
         self.result_count += 1
-        self.detectionResults.append(detectionResult)
+        self.detections.append(detection)
 
-        if not detectionResult.get_success():
+        if not detection.get_success():
             self.fail_count += 1 
-            print(f"FAILURE: [{detectionResult.detection.name}]")
+            print(f"FAILURE: [{detection.name}]")
         else:
             self.pass_count += 1
-            print(f"SUCCESS: [{detectionResult.detection.name}]")
+            print(f"SUCCESS: [{detection.name}]")
 
         
         
@@ -74,7 +42,7 @@ class ResultsManager:
         
         #We keep the whole thing because it removes the need to duplicate
         #certain fields when generating the summary
-        self.detectionResults.append(detectionResult)
+        self.detections.append(detection)
     
 
 
@@ -99,22 +67,22 @@ class ResultsManager:
             self.endTime = timeit.default_timer()
         
         background = {}
-        background['detections'] = len(self.detectionResults)
-        background['detections_pass'] = len([d for d in self.detectionResults if d.get_success()])
-        background['detections_fail'] = len([d for d in self.detectionResults if not d.get_success()])
+        background['detections'] = len(self.detections)
+        background['detections_pass'] = len([d for d in self.detections if d.get_success()])
+        background['detections_fail'] = len([d for d in self.detections if not d.get_success()])
     
 
-        background['tests'] = sum([len(d.detection.test.tests) for d in self.detectionResults])
-        all_tests:list[Test] = []
+        background['tests'] = sum([d.get_num_tests() for d in self.detections])
+        all_tests:list[Union[None,UnitTestResult]] = []
         for detection in self.detections:
-            for test in detection.testFile.tests:
+            for test in detection.get_all_unit_test_results():
                 all_tests.append(test)
-        background['tests_pass'] = len([t for t in all_tests if t.result is not None and t.result.success == True])
-        background['tests_fail'] = len([t for t in all_tests if t.result is not None and t.result.success == False])
-        background['total_time'] = str(timedelta(seconds = round(self.endTime - self.startTime)))
+        background['tests_pass'] = len([t for t in all_tests if t is not None and t.success == True])
+        background['tests_fail'] = len([t for t in all_tests if t is None or t.success == False])
 
-        
-
+        duration = datetime.now() - self.start_time
+        duration_without_microseconds = duration - timedelta(microseconds=duration.microseconds)
+        background['total_time'] = str(duration_without_microseconds)
 
         return background
 
@@ -122,27 +90,56 @@ class ResultsManager:
         results = []
         for detection in self.detections:
             success = True
-            thisDetection = {"name"  : detection.detectionFile.name,
-                             "id"    : detection.detectionFile.id,
-                             "search": detection.detectionFile.search,
-                             "path"  : str(detection.detectionFile.path),
+            thisDetection = {"name"  : detection.name,
+                             "id"    : detection.id,
+                             "search": detection.search,
+                             "path"  : str(detection.file_path),
                              "tests" : []}
-            for test in detection.testFile.tests:
+            for test in detection.test.tests:
+                #Set all the default fields which we can show, even if there 
+                #is an error
+                test_success = False
+                test_result = {
+                        "name": test.name,
+                        "attack_data": [d.data for d in test.attack_data],
+                        "success": False,
+                        "logic": False,
+                        "noise": False,
+                        "resultCount": 0,
+                        "runDuration": 0,
+                        "missing_observables": []
+                    }
+                
                 if test.result is None:
-                    raise(Exception(f"Detection {detection.detectionFile.name}, Test {test.name} in file {detection.testFile.path} was None, but should not be!"))
-                testResult = {
-                    "name": test.name,
-                    "attack_data": [d.data for d in test.attack_data],
-                    "success": test.result.success,
-                    "logic": test.result.logic,
-                    "noise": test.result.noise,
-                    #"performance": test.result.performance,
-                    "resultCount": test.result.resultCount,
-                    "runDuration": test.result.runDuration,
-                    "missing_observables": test.result.missing_observables
-                }
-                thisDetection['tests'].append(testResult)
-                success = success and test.result.success
+                    #This test was not run for some reason
+                    message = "Test Result was None"
+                    test_result['message'] = message
+                elif test.result.job is None:
+                    message = "Test Result Job was None"
+                    test_result['message'] = message
+                elif test.result.exception:
+                    #There was an exception detection when running this test
+                    message = "Encountered Exception while running the test."
+                    test_result['message'] = message
+
+                
+
+                else:
+                    #If something unexpected happens, should we raise an exception?
+                    #if result.job is None:
+                    #    raise(Exception(f"Detection {detection.name}, Test {test.name} in file {detection.file_path} was None, but should not be!"))
+                    test_result["success"] = test.result.success,
+                    test_result["logic"] = test.result.logic,
+                    test_result["noise"] = test.result.noise,
+                    test_result["resultCount"] = float(test.result.job['resultCount']),
+                    test_result["runDuration"] = float(test.result.job['runDuration']),
+                    test_result["missing_observables"] = test.result.missing_observables
+                    test_success = test.result.success
+
+                thisDetection['tests'].append(test_result)
+
+                success = success and test_success
+            
             thisDetection['success'] = success
             results.append(thisDetection)
 

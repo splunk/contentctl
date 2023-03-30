9472aa4e10ee544ccf2acc8df4fbbc07fbf93879
decoderzhub <darin.j.manley@gmail.com>
2023-03-29 19:47:00 -0700

    update home layout

--- contentctl_gui/_🏠_Contentctl_GUI.py
+++ contentctl_gui/_🏠_Contentctl_GUI.py
@@ -1,9 +1,4 @@
-import openai
-import uuid
-import yaml
-import content_fields as content_fields
-import new_content_questions as new_content_questions
-import datetime, random, sys
+
 import streamlit as st
 
 st.set_page_config(
@@ -20,75 +15,31 @@ hide_streamlit_style = """
             """
 st.markdown(hide_streamlit_style, unsafe_allow_html=True)
 
+st.markdown(
+        """
 
-# def init_router():
-#     return stx.Router({"/": Contentctl_GUI, 
-#                        "/status": status,
-#                        "/detections_creation": detection_creation
-#                        })
-# def Contentctl_GUI():
-#     pass
+        #### ContentCtl GUI is an open-source webapp built specifically for creating security content.
+        ### **👈 Click the 🕵🏽 Detection Creation on the left** to create your detection
 
-# def detection_creation():
-#     detection_page
+    """
+    )
 
-# def status():
-#     status_page
+col1, col2, col3 = st.columns([.2,.5,.2])
+    
+with col1:
+    pass
 
+with col2:
+    st.markdown(
+        """
+        ### Want to learn more?
 
-# router = init_router()
-# router.show_route_view()
+        - Check out [Splunk ContentCtl](https://github.com/splunk/contentctl)
+        - Jump into our [documentation](https://github.com/splunk/contentctl/wiki)
+        - Having issues? Sumbit a [new issue
+          ](https://github.com/splunk/contentctl/issues))
+        """ )
+with col3:
+    pass
 
-st.write("welcome!")
-
-
-# c1, c2, c3 = st.columns(3)
-
-# with c1:
-#     st.header("Current route")
-#     current_route = router.get_url_route()
-#     st.write(f"{current_route}")
-# with c2:
-#     st.header("Set route")
-#     new_route = st.text_input("route")
-#     if st.button("Route now!"):
-#         router.route(new_route)
-# with c3:
-#     st.header("Session state")
-#     st.write(st.session_state)
-
-
-import websocket
-import threading
-
-# Define the WebSocket server URL
-WS_URL = "ws://127.0.0.1:6807"
-
-def on_message(ws, message):
-    # Handle incoming messages from the client
-    print("Received message:", message)
-
-def on_error(ws, error):
-    # Handle errors from the WebSocket connection
-    print("Error:", error)
-
-def on_close(ws, close_status_code, close_reason):
-    # Handle WebSocket connection close events
-    print("WebSocket closed with status code:", close_status_code, "reason:", close_reason)
-
-def on_open(ws):
-    # Handle WebSocket connection open events
-    print("WebSocket connected")
-
-def start_websocket_server():
-    # Start the WebSocket server in a separate thread
-    websocket.enableTrace(True)
-    ws = websocket.WebSocketApp(WS_URL,
-                                on_message=on_message,
-                                on_error=on_error,
-                                on_close=on_close)
-    ws.on_open = on_open
-    ws.run_forever()
-
-threading.Thread(target=start_websocket_server).start()
 
--- contentctl_gui/pages/1_📊_Status.py
+++ contentctl_gui/pages/1_📊_Status.py
@@ -1,39 +1,19 @@
 import streamlit as st
-import requests
-import websocket, json
+import contentctl.contentctl
+import argparse, subprocess
+import os
+print(os.getcwd())
 
-# Define the WebSocket server URL
-WS_URL = "ws://127.0.0.1:6807"
+# subprocess.Popen("mkdir temp",)
 
-# Define a function to handle incoming WebSocket messages
-def on_message(message):
-    # Parse the message as JSON
-    data = json.loads(message)
+parser = argparse.ArgumentParser()
+parser.add_argument("--path", default=".", help="Path to directory")
 
-    # Do something with the data
-    st.write(data)
+args = parser.parse_args()
 
-# Connect to the WebSocket server
-ws = websocket.WebSocketApp(WS_URL, on_message=on_message)
-ws.run_forever()
-
-value = st.experimental_get_query_params()
-st.write("say something!")
-st.write(value)
+def start(args):
+    contentctl.start(args)
 
 
-# # Define the HTTP endpoint URL
-# URL = "http://localhost:7999/data"
+st.button("start", on_click=start(args))
 
-# # Make a GET request to the endpoint
-# response = requests.get(URL)
-
-# # Check if the request was successful
-# if response.status_code == 200:
-#     # Extract the data from the response
-#     data = response.text
-
-#     # Display the data in the Streamlit app
-#     st.write(data, unsafe_allow_html=True)
-# else:
-#     st.error("Failed to retrieve data from HTTP endpoint")

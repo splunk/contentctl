c4f49ba4319fa513902aa8d62c064f3765dea25f
pyth0n1c <emcginnis@splunk.com>
2023-10-31 16:57:48 -0700

    Fix badge generation code

--- contentctl/output/svg_output.py
+++ contentctl/output/svg_output.py
@@ -4,30 +4,32 @@ import pathlib
 from contentctl.objects.enums import SecurityContentType
 from contentctl.output.jinja_writer import JinjaWriter
 from contentctl.objects.config import Config
-
+from contentctl.objects.enums import DetectionStatus
 class SvgOutput():
 
     def writeObjects(self, objects: list, path: str, type: SecurityContentType = None) -> None:
         
-        detections_tmp = objects
-        detection_without_test = 0    
+        detections_tmp = objects    
 
         output_path = pathlib.Path(path)
 
-        detections = []
+        tested_detections = []
+        untested_detections = []
         obj = dict()
-
+        
         for detection in detections_tmp:
-            if not detection.status == "deprecated":
-                detections.append(detection)
+            if detection.status == DetectionStatus.production.value:
+                tested_detections.append(detection)
+            else: 
+                untested_detections.append(detection)
 
-                if not detection.tests and not detection.experimental:
-                    detection_without_test = detection_without_test + 1
-
-
-        obj['count'] = len(detections) 
-        obj['coverage'] = (obj['count'] - detection_without_test)/obj['count']
-        obj['coverage'] = "{:.0%}".format(obj['coverage'])
+        
+        obj['count'] = len(tested_detections) + len(untested_detections)
+        if obj['count'] == 0:
+            obj['coverage'] = "NaN"
+        else:
+            obj['coverage'] = len(tested_detections) / obj['count']
+            obj['coverage'] = "{:.0%}".format(obj['coverage'])
 
         JinjaWriter.writeObject('detection_count.j2', os.path.join(output_path, 'detection_count.svg'), obj)
         JinjaWriter.writeObject('detection_coverage.j2', os.path.join(output_path, 'detection_coverage.svg'), obj)

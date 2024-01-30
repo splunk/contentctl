01beb41cea0b7cce4a98cf1fae509d32c6b9cb6f
pyth0n1c <emcginnis@splunk.com>
2024-01-30 13:51:46 -0800

    Updated detection generator for newest yml format

--- contentctl/input/new_content_generator.py
+++ contentctl/input/new_content_generator.py
@@ -34,9 +34,8 @@ class NewContentGenerator():
             self.output_dto.obj['version'] = 1
             self.output_dto.obj['date'] = datetime.today().strftime('%Y-%m-%d')
             self.output_dto.obj['author'] = answers['detection_author']
+            self.output_dto.obj['data_source'] = answers['data_source']
             self.output_dto.obj['type'] = answers['detection_type']
-            self.output_dto.obj['datamodel'] = answers['datamodels']
-            self.output_dto.obj['datamodel'] = answers['datamodels']
             self.output_dto.obj['description'] = 'UPDATE_DESCRIPTION'   
             file_name = self.output_dto.obj['name'].replace(' ', '_').replace('-','_').replace('.','_').replace('/','_').lower()
             self.output_dto.obj['search'] = answers['detection_search'] + ' | `' + file_name + '_filter`'
@@ -46,21 +45,31 @@ class NewContentGenerator():
             self.output_dto.obj['tags'] = dict()
             self.output_dto.obj['tags']['analytic_story'] = ['UPDATE_STORY_NAME']
             self.output_dto.obj['tags']['asset_type'] = 'UPDATE asset_type'
-            self.output_dto.obj['tags']['cis20'] = ['CIS 3', 'CIS 5', 'CIS 16']
             self.output_dto.obj['tags']['confidence'] = 'UPDATE value between 1-100'
-            self.output_dto.obj['tags']['context'] = ['Update context']
-            self.output_dto.obj['tags']['dataset'] = ['UPDATE_DATASET_URL']
             self.output_dto.obj['tags']['impact'] = 'UPDATE value between 1-100'
-            self.output_dto.obj['tags']['kill_chain_phases'] = answers['kill_chain_phases']
             self.output_dto.obj['tags']['message'] = 'UPDATE message'
             self.output_dto.obj['tags']['mitre_attack_id'] = [x.strip() for x in answers['mitre_attack_ids'].split(',')]
-            self.output_dto.obj['tags']['nist'] = ['DE.CM']
             self.output_dto.obj['tags']['observable'] = [{'name': 'UPDATE', 'type': 'UPDATE', 'role': ['UPDATE']}]
             self.output_dto.obj['tags']['product'] = ['Splunk Enterprise','Splunk Enterprise Security','Splunk Cloud']
             self.output_dto.obj['tags']['required_fields'] = ['UPDATE']
             self.output_dto.obj['tags']['risk_score'] = 'UPDATE (impact * confidence)/100'
             self.output_dto.obj['tags']['security_domain'] = answers['security_domain']
-            #self.output_dto.obj['source'] = answers['detection_kind']
+            self.output_dto.obj['tags']['cve'] = ['UPDATE WITH CVE(S) IF APPLICABLE']
+            
+            #generate the tests section
+            self.output_dto.obj['tests'] = [
+                {
+                    'name': "True Positive Test",
+                    'attack_data': [ 
+                        {
+                        'data': "Enter URL for Dataset Here.  This may also be a relative or absolute path on your local system for testing.",
+                        "sourcetype": "UPDATE SOURCETYPE",
+                        "source": "UPDATE SOURCE"
+                        }
+                    ]
+                }
+            ]
+            
         
 
         elif input_dto.type == SecurityContentType.stories:

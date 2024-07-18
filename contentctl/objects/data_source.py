cfe1b0c658789acefd93a0389f47e335c4f83469
Patrick <pbareiss@splunk.com>
2024-07-18 13:05:38 +0200

    Enabled file_name match name validator to SecurityContentObjectAbstract

--- contentctl/objects/data_source.py
+++ contentctl/objects/data_source.py
@@ -10,20 +10,8 @@ class DataSource(SecurityContentObject):
     separator: Optional[str] = None
     configuration: Optional[str] = None
     supported_TA: Optional[list] = None
-    event_names: Optional[list] = None
     fields: Optional[list] = None
     example_log: Optional[str] = None
 
-    event_sources: Optional[list[EventSource]] = None
+    event_sources: Optional[list] = None
 
-
-    def model_post_init(self, ctx:dict[str,Any]):
-        context = ctx.get("output_dto")
-        
-        if self.event_names:
-            self.event_sources = []
-            for event_source in context.event_sources:
-                if any(event['event_name'] == event_source.name for event in self.event_names):
-                    self.event_sources.append(event_source)
-
-        return self

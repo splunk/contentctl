65e60106d6902dd8ed47c8c4d024ff3a1773978e
ljstella <lstella@splunk.com>
2024-07-23 11:25:24 -0500

    Remove extra line

--- contentctl/actions/new_content.py
+++ contentctl/actions/new_content.py
@@ -32,7 +32,6 @@ class NewContent:
         answers['status'] = "production" #start everything as production since that's what we INTEND the content to become   
         answers['description'] = 'UPDATE_DESCRIPTION'   
         file_name = answers['name'].replace(' ', '_').replace('-','_').replace('.','_').replace('/','_').lower()
-        
         answers['search'] = answers['detection_search'] + ' | `' + file_name + '_filter`'
         del answers['detection_search']
         answers['how_to_implement'] = 'UPDATE_HOW_TO_IMPLEMENT'

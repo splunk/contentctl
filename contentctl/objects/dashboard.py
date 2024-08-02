d0e0a2979720ac505d7ec08419d95d9f58089bfa
pyth0n1c <emcginnis@splunk.com>
2024-08-01 17:00:49 -0700

    split json for dashboard into a separate file

--- contentctl/objects/dashboard.py
+++ contentctl/objects/dashboard.py
@@ -33,38 +33,46 @@ class Dashboard(SecurityContentObject):
     @model_validator(mode="before")
     @classmethod
     def validate_fields_from_json(cls, data:Any)->Any:
-        raw_json = data.get("json_obj", None)
+        yml_file_name:str|None = data.get("file_path", None)
+        if yml_file_name is None:
+            raise ValueError("File name not passed to dashboard constructor")
+        yml_file_path = pathlib.Path(yml_file_name)
+        json_file_path = yml_file_path.with_suffix(".json")
+
+        if not json_file_path.is_file():
+            raise ValueError(f"Required file {json_file_path} does not exist.")
         
-        try:
-            json_obj = json.loads(raw_json)
-        except Exception as e:
-            raise ValueError(f"Error getting field 'json_obj'. Field does not contain valid JSON: {str(e)}")
+        with open(json_file_path,'r') as jsonFilePointer:
+            try:
+                json_obj:dict[str,Any] = json.load(jsonFilePointer)
+            except Exception as e:
+                raise ValueError(f"Unable to load data from {json_file_path}: {str(e)}")
 
         name_from_file = data.get("name",None)
         name_from_json  = json_obj.get("title",None)
-        description_from_file = data.get("description",None)
-        description_from_json = json_obj.get("description",None)
 
         errors:list[str] = []
         if name_from_json is None:
-            errors.append("'title' field is missing from field 'json_object'")
-        elif name_from_json is not None and name_from_file is None:
-            name_from_file = name_from_json
+            errors.append(f"'title' field is missing from {json_file_path}")
         elif name_from_json != name_from_file:
             errors.append(f"title 'json_object' is '{name_from_json}', but the name defined in the YML is '{name_from_file}'. These two must match.")
+        
+        
+        if data.get("description",None) is not None:
+            raise ValueError(f"The description field should not be defined in {yml_file_path}, is is read from {json_file_path}")
+        
+        description_from_json = json_obj.get("description",None)
 
         if description_from_json is None:
             errors.append("'description' field is missing from field 'json_object'")
-        elif description_from_json is not None and description_from_file is None:
-            description_from_file = description_from_file
-        elif description_from_json != description_from_file:
-            errors.append(f"description in 'json_object' is '{description_from_json}', but the description defined in the YML is '{description_from_file}'. These two must match.")
         
-        data['name'] = name_from_json
-        data['description'] = description_from_json        
         if len(errors) > 0 :
             err_string = "\n".join(errors)
             raise ValueError(f"Error(s) validating dashboard:\n{err_string}")
+        
+        data['name'] = name_from_file
+        data['description'] = description_from_json        
+        data['json_obj'] = json.dumps(json_obj)
         return data
 
     

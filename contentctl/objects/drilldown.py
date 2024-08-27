0b48ce4250c43562671bd60f35a807034c371171
pyth0n1c <87383215+pyth0n1c@users.noreply.github.com>
2024-08-27 13:28:40 -0700

    Relax requirement on search string from drilldown

--- contentctl/objects/drilldown.py
+++ contentctl/objects/drilldown.py
@@ -1,6 +1,6 @@
 from pydantic import BaseModel, Field
 class Drilldown(BaseModel):
     name: str = Field(...,min_length=5)
-    search: str = Field(...,description= "The drilldown search. The drilldown MUST begin with the | character followed by a space.", pattern=r"^\|\s+.*")
+    search: str = Field(..., description="The text of a drilldown search. This must be valid SPL." min_length=1)
     earliest_offset:str = "$info_min_time$"
     latest_offset:str = "$info_max_time$"

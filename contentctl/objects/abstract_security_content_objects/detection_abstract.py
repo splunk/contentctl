d6b848eb97ae1bf451aed10681b4997b0ac35c4d
ljstella <lstella@splunk.com>
2024-11-12 11:24:14 -0600

    Another None case

--- contentctl/objects/abstract_security_content_objects/detection_abstract.py
+++ contentctl/objects/abstract_security_content_objects/detection_abstract.py
@@ -805,6 +805,11 @@ class Detection_Abstract(SecurityContentObject):
                     "Detection does not have a matching RBA deployment config, the RBA portion should be omitted."
                 )
         else:
+            print(self.name)
+            if self.rba is None:
+                raise ValueError(
+                    "Detection is expected to have an RBA object based on its deployment config"
+                )
             if len(self.rba.risk_objects) > 0: # type: ignore
                 return self
             else:

cc51953d3ea3bc3a6d8b9f78546f8cb41894969e
pyth0n1c <emcginnis@splunk.com>
2024-12-23 11:12:54 -0800

    More cleanup with different classes

--- contentctl/objects/lookup.py
+++ contentctl/objects/lookup.py
@@ -1,10 +1,13 @@
 from __future__ import annotations
-from pydantic import field_validator, ValidationInfo, model_validator, FilePath, model_serializer, Field
-from typing import TYPE_CHECKING, Optional, Any, Union, Literal
+
+from pydantic import field_validator, ValidationInfo, model_validator, FilePath, model_serializer, Field, NonNegativeInt, computed_field
+from enum import StrEnum, auto
+from typing import TYPE_CHECKING, Optional, Any, Union, Literal, Annotated, Self
 import re
 import csv
-from enum import StrEnum
 import abc
+from functools import cached_property
+import pathlib
 if TYPE_CHECKING:
     from contentctl.input.director import DirectorOutputDto
     from contentctl.objects.config import validate
@@ -26,17 +29,24 @@ LOOKUPS_TO_IGNORE.add("=")
 LOOKUPS_TO_IGNORE.add("other_lookups") 
 
 
-# TODO (#220): Split Lookup into 2 classes
-class Lookup(SecurityContentObject, abc.ABC):
-    #collection will always be the name of the lookup
+class Lookup_Type(StrEnum):
+    csv = auto()
+    kvstore = auto()
+    mlmodel = auto()
 
-    fields_list: Optional[str] = None
-    filename: Optional[FilePath] = None
+
+
+# TODO (#220): Split Lookup into 2 classes
+class Lookup(SecurityContentObject, abc.ABC):    
     default_match: Optional[bool] = None
-    match_type: Optional[str] = None
-    min_matches: Optional[int] = None
-    case_sensitive_match: Optional[bool] = None
+    # Per the documentation for transforms.conf, EXACT should not be specified in this list,
+    # so we include only WILDCARD and CIDR
+    match_type: list[Annotated[str, Field(pattern=r"(^WILDCARD|CIDR)\(.+\)$")]] = Field(default=[])
+    min_matches: None | NonNegativeInt = Field(default=None)
+    max_matches: None | Annotated[NonNegativeInt, Field(ge=1, le=1000)] = Field(default=None)    
+    case_sensitive_match: None | bool = Field(default=None)
     
+
 
     @model_serializer
     def serialize_model(self):
@@ -45,13 +55,11 @@ class Lookup(SecurityContentObject, abc.ABC):
 
         #All fields custom to this model
         model= {
-            "filename": self.filename.name if self.filename is not None else None,
+            
             "default_match": "true" if self.default_match is True else "false",
             "match_type": self.match_type,
             "min_matches": self.min_matches,
             "case_sensitive_match": "true" if self.case_sensitive_match is True else "false",
-            "collection": self.collection,
-            "fields_list": self.fields_list
         }
         
         #return the model
@@ -69,78 +77,9 @@ class Lookup(SecurityContentObject, abc.ABC):
         return data
 
 
-    def model_post_init(self, ctx:dict[str,Any]):
-        if not self.filename:
-            return
-        import pathlib
-        filenamePath = pathlib.Path(self.filename)
-        
-        if filenamePath.suffix not in [".csv", ".mlmodel"]:
-            raise ValueError(f"All Lookup files must be CSV files and end in .csv.  The following file does not: '{filenamePath}'")
-        
-        
-
-        if filenamePath.suffix == ".mlmodel":
-            # Do not need any additional checks for an mlmodel file
-            return
-
-        # https://docs.python.org/3/library/csv.html#csv.DictReader
-        # Column Names (fieldnames) determine by the number of columns in the first row.
-        # If a row has MORE fields than fieldnames, they will be dumped in a list under the key 'restkey' - this should throw an Exception
-        # If a row has LESS fields than fieldnames, then the field should contain None by default. This should also throw an exception.    
-        csv_errors:list[str] = []
-        with open(filenamePath, "r") as csv_fp:
-            RESTKEY = "extra_fields_in_a_row"
-            csv_dict = csv.DictReader(csv_fp, restkey=RESTKEY)            
-            if csv_dict.fieldnames is None:
-                raise ValueError(f"Error validating the CSV referenced by the lookup: {filenamePath}:\n\t"
-                                 "Unable to read fieldnames from CSV. Is the CSV empty?\n"
-                                 "  Please try opening the file with a CSV Editor to ensure that it is correct.")
-            # Remember that row 1 has the headers and we do not iterate over it in the loop below
-            # CSVs are typically indexed starting a row 1 for the header.
-            for row_index, data_row in enumerate(csv_dict):
-                row_index+=2
-                if len(data_row.get(RESTKEY,[])) > 0:
-                    csv_errors.append(f"row [{row_index}] should have [{len(csv_dict.fieldnames)}] columns,"
-                                      f" but instead had [{len(csv_dict.fieldnames) + len(data_row.get(RESTKEY,[]))}].")
-                
-                for column_index, column_name in enumerate(data_row):
-                    if data_row[column_name] is None:
-                        csv_errors.append(f"row [{row_index}] should have [{len(csv_dict.fieldnames)}] columns, "
-                                          f"but instead had [{column_index}].")
-        if len(csv_errors) > 0:
-            err_string = '\n\t'.join(csv_errors)
-            raise ValueError(f"Error validating the CSV referenced by the lookup: {filenamePath}:\n\t{err_string}\n"
-                             f"  Please try opening the file with a CSV Editor to ensure that it is correct.")
-    
-        return
-    
-        
-    @field_validator('match_type')
-    @classmethod
-    def match_type_valid(cls, v: Union[str,None], info: ValidationInfo):
-        if not v:
-            #Match type can be None and that's okay
-            return v
-
-        if not (v.startswith("WILDCARD(") or v.endswith(")")) :
-            raise ValueError(f"All match_types must take the format 'WILDCARD(field_name)'. The following file does not: '{v}'")
-        return v
-
-
-    #Ensure that exactly one of location or filename are defined
-    @model_validator(mode='after')
-    def ensure_mutually_exclusive_fields(self)->Lookup:
-        if self.filename is not None and self.collection is not None:
-            raise ValueError("filename and collection cannot be defined in the lookup file.  Exactly one must be defined.")
-        elif self.filename is None and self.collection is None:
-            raise ValueError("Neither filename nor collection were defined in the lookup file.  Exactly one must "
-                             "be defined.")
-
-
-        return self
     
     
+        
     @staticmethod
     def get_lookups(text_field: str, director:DirectorOutputDto, ignore_lookups:set[str]=LOOKUPS_TO_IGNORE)->list[Lookup]:
         inputLookupsToGet = set(re.findall(r'inputlookup(?:\s*(?:(?:append|strict|start|max)\s*=\s*(?:true|t|false|f))){0,4}\s+([^\s]+)', text_field))
@@ -164,34 +103,129 @@ class Lookup(SecurityContentObject, abc.ABC):
             
         return my_lookups
 
-class Lookup_external_type(StrEnum):
-    PYTHON = "python"
-    EXECUTABLE = "executable" 
-    KVSTORE = "kvstore"
-    GEO = "geo"
-    GEO_HEX = "geo_hex"
-
-class ExternalLookup(Lookup, abc.ABC):
-    fields_list: list[str] = Field(...,min_length=1)
-    external_type: Lookup_external_type = Field(...)
-    
-class PythonLookup(ExternalLookup):
-    external_type = Lookup_external_type.PYTHON
-    
-class ExecutableLookup(ExternalLookup):
-    external_type = Lookup_external_type.EXECUTABLE
-
-class KVStoreLookup(ExternalLookup):
-    external_type = Lookup_external_type.KVSTORE
-
-class GeoLookup(ExternalLookup):
-    external_type = Lookup_external_type.GEO
-
-class GeoHexLookup(ExternalLookup):
-    external_type = Lookup_external_type.GEO_HEX
 
 
 
-    
+
+
 class CSVLookup(Lookup):
-    filename: FilePath = Field(...)
+    lookup_type: Literal[Lookup_Type.csv]
+    
+
+    @model_validator(mode="after")
+    def ensure_lookup_file_exists(self)->Self:
+        if not self.filename.exists():
+            raise ValueError(f"Expected lookup filename {self.filename} does not exist")
+        return self
+
+    @computed_field
+    @cached_property
+    def filename(self)->FilePath:
+        if self.file_path is None:
+            raise ValueError("Cannot get the filename of the lookup CSV because the YML file_path attribute is None")
+        
+        csv_file = self.file_path.parent / f"{self.file_path.stem}.csv"
+        return csv_file
+    
+    @computed_field
+    @cached_property
+    def app_filename(self)->FilePath:
+        '''
+        We may consider two options:
+        1. Always apply the datetime stamp to the end of the file. This makes the code easier
+        2. Only apply the datetime stamp if it is version > 1.  This makes the code a small fraction
+        more complicated, but preserves longstanding CSV that have not been modified in a long time
+        '''
+        return pathlib.Path(f"{self.filename.stem}_{self.date.year}{self.date.month:02}{self.date.day:02}.csv")
+
+
+
+    @model_serializer
+    def serialize_model(self):
+        #Call parent serializer
+        super_fields = super().serialize_model()
+
+        #All fields custom to this model
+        model= {
+            "filename": self.filename.name
+        }
+        
+        #return the model
+        model.update(super_fields)
+        return model
+    
+    @model_validator(mode="after")
+    def ensure_correct_csv_structure(self)->Self:
+        
+        
+        if self.filename.suffix != ".csv":
+            raise ValueError(f"All Lookup files must be CSV files and end in .csv.  The following file does not: '{self.filename}'")
+        
+    
+
+        # https://docs.python.org/3/library/csv.html#csv.DictReader
+        # Column Names (fieldnames) determine by the number of columns in the first row.
+        # If a row has MORE fields than fieldnames, they will be dumped in a list under the key 'restkey' - this should throw an Exception
+        # If a row has LESS fields than fieldnames, then the field should contain None by default. This should also throw an exception.    
+        csv_errors:list[str] = []
+        with open(self.filename, "r") as csv_fp:
+            RESTKEY = "extra_fields_in_a_row"
+            csv_dict = csv.DictReader(csv_fp, restkey=RESTKEY)            
+            if csv_dict.fieldnames is None:
+                raise ValueError(f"Error validating the CSV referenced by the lookup: {self.filename}:\n\t"
+                                 "Unable to read fieldnames from CSV. Is the CSV empty?\n"
+                                 "  Please try opening the file with a CSV Editor to ensure that it is correct.")
+            # Remember that row 1 has the headers and we do not iterate over it in the loop below
+            # CSVs are typically indexed starting a row 1 for the header.
+            for row_index, data_row in enumerate(csv_dict):
+                row_index+=2
+                if len(data_row.get(RESTKEY,[])) > 0:
+                    csv_errors.append(f"row [{row_index}] should have [{len(csv_dict.fieldnames)}] columns,"
+                                      f" but instead had [{len(csv_dict.fieldnames) + len(data_row.get(RESTKEY,[]))}].")
+                
+                for column_index, column_name in enumerate(data_row):
+                    if data_row[column_name] is None:
+                        csv_errors.append(f"row [{row_index}] should have [{len(csv_dict.fieldnames)}] columns, "
+                                          f"but instead had [{column_index}].")
+        if len(csv_errors) > 0:
+            err_string = '\n\t'.join(csv_errors)
+            raise ValueError(f"Error validating the CSV referenced by the lookup: {self.filename}:\n\t{err_string}\n"
+                             f"  Please try opening the file with a CSV Editor to ensure that it is correct.")
+    
+        return self
+
+
+
+class KVStoreLookup(Lookup):
+    lookup_type: Literal[Lookup_Type.kvstore]
+    collection: str = Field(description="Name of the KVStore Collection. Note that collection MUST equal the name.")
+    fields: list[str] = Field(description="The names of the fields/headings for the KVStore.", min_length=1)
+
+
+    @model_validator(mode="after")
+    def validate_collection(self)->Self:
+        if self.collection != self.name:
+            raise ValueError("Collection MUST be the same as Name of the lookup, but they do not match")
+        return self
+
+    @model_serializer
+    def serialize_model(self):
+        #Call parent serializer
+        super_fields = super().serialize_model()
+
+        #All fields custom to this model
+        model= {
+            "collection": self.collection,
+            "fields_list": ", ".join(self.fields)
+        }
+        
+        #return the model
+        model.update(super_fields)
+        return model
+
+class MlModel(Lookup):
+    lookup_type: Literal[Lookup_Type.mlmodel]
+    
+
+
+

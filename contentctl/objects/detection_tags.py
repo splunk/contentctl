4216c45e2fdf5172d3f196d8736b424be49537f2
pyth0n1c <emcginnis@splunk.com>
2025-02-11 12:37:44 -0800

    error into message for now

--- contentctl/objects/detection_tags.py
+++ contentctl/objects/detection_tags.py
@@ -196,7 +196,11 @@ class DetectionTags(BaseModel):
             error_string = "\n".join(
                 str(e) for e in subtype_and_parent_exist_exceptions
             )
-            raise ValueError(
+            # raise ValueError(
+            #     "Overlapping MITRE Attack ID Tactics and Subtactics may not be defined. "
+            #     f"Remove the tactic and keep the subtactic:\n{error_string}"
+            # )
+            print(
                 "Overlapping MITRE Attack ID Tactics and Subtactics may not be defined. "
                 f"Remove the tactic and keep the subtactic:\n{error_string}"
             )

a796dbf4973da19faf87dd64aed89d1b4f5bb128
pyth0n1c <emcginnis@splunk.com>
2025-02-13 14:31:15 -0800

    Improve typing and output for lookup default_match field.

--- contentctl/objects/lookup.py
+++ contentctl/objects/lookup.py
@@ -69,7 +69,10 @@ class Lookup_Type(StrEnum):
 
 # TODO (#220): Split Lookup into 2 classes
 class Lookup(SecurityContentObject, abc.ABC):
-    default_match: Optional[bool] = None
+    default_match: str = Field(default='', description="This field is given a default value of ''"
+                                "because it is the default value specified in the transforms.conf "
+                                "docs. Giving it a type of str rather than str | None simplifies "
+                                "the typing for the field.")
     # Per the documentation for transforms.conf, EXACT should not be specified in this list,
     # so we include only WILDCARD and CIDR
     match_type: list[Annotated[str, Field(pattern=r"(^WILDCARD|CIDR)\(.+\)$")]] = Field(
@@ -88,7 +91,7 @@ class Lookup(SecurityContentObject, abc.ABC):
 
         # All fields custom to this model
         model = {
-            "default_match": "true" if self.default_match is True else "false",
+            "default_match": self.default_match,
             "match_type": self.match_type_to_conf_format,
             "min_matches": self.min_matches,
             "max_matches": self.max_matches,

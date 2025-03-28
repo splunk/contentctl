42fd4e7e644345612ddd5d50d15fa193c03e5c92
Eric McGinnis <emcginnis@splunk.com>
2025-03-28 11:56:07 -0700

    fix formatting with ruff

--- contentctl/input/yml_reader.py
+++ contentctl/input/yml_reader.py
@@ -1,7 +1,8 @@
-from typing import Dict, Any
-import yaml
-import sys
 import pathlib
+import sys
+from typing import Any, Dict
+
+import yaml
 
 
 class YmlReader:
@@ -11,30 +12,29 @@ class YmlReader:
         add_fields: bool = True,
         STRICT_YML_CHECKING: bool = False,
     ) -> Dict[str, Any]:
-        try: 
+        try:
             file_handler = open(file_path, "r", encoding="utf-8")
         except OSError as exc:
-            print(f"\nThere was an unrecoverable error when opening the file '{file_path}' - we will exit immediately:\n{str(exc)}")
+            print(
+                f"\nThere was an unrecoverable error when opening the file '{file_path}' - we will exit immediately:\n{str(exc)}"
+            )
             sys.exit(1)
 
-            
-        
             # The following code can help diagnose issues with duplicate keys or
             # poorly-formatted but still "compliant" YML.  This code should be
             # enabled manually for debugging purposes. As such, strictyaml
             # library is intentionally excluded from the contentctl requirements
 
-        
         try:
             if STRICT_YML_CHECKING:
                 # This is an extra level of verbose parsing that can be
                 # enabled for debugging purpose. It is intentionally done in
                 # addition to the regular yml parsing
                 import strictyaml
+
                 strictyaml.dirty_load(file_handler.read(), allow_flow_style=True)
                 file_handler.seek(0)
 
-            
             # Ideally we should use
             # from contentctl.actions.new_content import NewContent
             # and use NewContent.UPDATE_PREFIX,
@@ -49,9 +49,10 @@ class YmlReader:
             yml_obj = yaml.load(data, Loader=yaml.CSafeLoader)
 
         except yaml.YAMLError as exc:
-            print(f"\nThere was an unrecoverable YML Parsing error when reading or parsing the file '{file_path}' - we will exit immediately:\n{str(exc)}")
+            print(
+                f"\nThere was an unrecoverable YML Parsing error when reading or parsing the file '{file_path}' - we will exit immediately:\n{str(exc)}"
+            )
             sys.exit(1)
-
 
         if add_fields is False:
             return yml_obj

a3faba717dcc3010ce5c3e030fcd5134c0c91722
0xC0FFEEEE <119874251+0xC0FFEEEE@users.noreply.github.com>
2025-04-09 22:54:28 +0100

    basic email validation

--- contentctl/objects/email.py
+++ contentctl/objects/email.py
@@ -1,11 +1,20 @@
 from __future__ import annotations
 
 from abc import ABC
+from typing import Any
 
-from pydantic import BaseModel
+from pydantic import BaseModel, model_validator
 
 
 class EmailObject(BaseModel, ABC):
     to: str
     subject: str
     message: str
+
+    @model_validator(mode="before")
+    # Validate the email address
+    def validate_email(cls, data: str) -> str:
+        if data.get("to"):
+            if "@" not in data.get("to"):
+                raise ValueError("Invalid email address")
+            return data

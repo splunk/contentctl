1f4de5492ab8cea79af0e27d6b1c32bf38627fd8
Casey McGinley <cmcginley@splunk.com>
2025-05-09 17:56:14 -0700

    upping the timeout for cms_parser

--- contentctl/objects/content_versioning_service.py
+++ contentctl/objects/content_versioning_service.py
@@ -234,7 +234,7 @@ class ContentVersioningService(BaseModel):
         elapsed_sleep_time = 0
         num_tries = 0
         time_to_sleep = 2**num_tries
-        max_sleep = 600
+        max_sleep = 720
 
         # Loop until timeout
         while elapsed_sleep_time < max_sleep:
